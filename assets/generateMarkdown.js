0affc0106411828932382e725d794e0940ad46ce
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-01-06 17:49:18 -0600

    generate markdown for readme

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,19 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return ` [![license](https://img.shields.io/badge/license-${license}-blue.svg)]`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `- License(#license)`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    This project utilizes the ${license} license.`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  ${renderLicenseLink(data.license)}
+  - [Questions](#questions)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installion}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.testing}
+  ${renderLicenseSection(data.license)}
+  ## Questions
+  If you have any questions, please check out my GitHub at ${data.userNameGitHub}, or please email me at ${data.email}.
 `;
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
-    // require 'fs' and 'inquirer' to use those programs//
-    // require generateMarkdown.js access that file paths//
+// require 'fs' and 'inquirer' to use those programs//
+// require generateMarkdown.js access that file paths//
 let fs = require('fs');
 let inquirer = require('inquirer');
 let createReadMe = require('./assets/generateMarkdown.js');
@@ -33,6 +33,11 @@ const questions = [
         message: 'Guidelines for my project?',
     },
     {
+        type: 'input',
+        name: 'testing',
+        message: 'Insctructions for testing my project?',
+    },
+    {
         type: 'list',
         name: 'license',
         message: 'Select license for my project?',
@@ -53,16 +58,16 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-    err ? console.log(err) : console.log('Success!')
+        err ? console.log(err) : console.log('Success!')
     );
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then((data) => {
-        writeToFile('README.md', createReadMe(data));
-    });
+        .then((data) => {
+            writeToFile('README.md', createReadMe(data));
+        });
 }
 
 // Function call to initialize app
